4d4909c13a1342c9d078f9ce84ce4be41ee28ec5
palak00181-afk <palak00181@gmail.com>
2026-04-28 23:44:43 +0530

    uc7

---
+++ src/uc7.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class uc7 {
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+
+    static char computerSymbol = 'O';
+
+    public static void main(String[] args) {
+        computerMove();
+    }
+
+    static void computerMove() {
+    }
+}
