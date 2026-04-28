79488a9a8f8f851ba1d0f5449f85719fdbced02d
palak00181-afk <palak00181@gmail.com>
2026-04-28 23:39:38 +0530

    uc5

---
+++ src/uc5.java
@@ -0,0 +1,18 @@
+public class uc5 {
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+
+    public static void main(String[] args) {
+        System.out.println(isValidMove(1, 1));
+    }
+
+    static boolean isValidMove(int row, int col) {
+        if (row < 0 || row >= 3 || col < 0 || col >= 3) {
+            return false;
+        }
+        return board[row][col] == '-';
+    }
+}
