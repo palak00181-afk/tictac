dc8fb8fd4332867ef07f6ff1326dc7c98515edfc
palak00181-afk <palak00181@gmail.com>
2026-04-28 23:48:48 +0530

    uc9

---
+++ src/uc9.java
@@ -0,0 +1,10 @@
+public class uc9 {
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        System.out.println(hasWon('X'));
+    }
+
+    static boolean hasWon(char symbol) {
+    }
+}
