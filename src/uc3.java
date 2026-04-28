7cd9cfa9a66ff062770020ba194d256ecc314d91
palak00181-afk <palak00181@gmail.com>
2026-04-28 23:34:00 +0530

    uc3

---
+++ src/uc3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class uc3 {
+    public static void main(String[] args) {
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
+    }
+
+    static int getUserSlot() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = scanner.nextInt();
+        return slot;
+    }
+
+}
